5674656d75347cf9776b45c42727901c482167bc
Ryoyan <553564035@qq.com>
2015-08-25 00:09:55 +0800

    jobmaket1

--- Job/app/src/main/java/com/mardin/job/fragments/zhiwei/HomeFragment.java
+++ Job/app/src/main/java/com/mardin/job/fragments/zhiwei/HomeFragment.java
@@ -17,7 +17,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_topicdetail, container, false);
+        return inflater.inflate(R.layout.activity_question_main, container, false);
     }
 }
 
