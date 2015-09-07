8e48b70105bc96b8ce9564fb1db625af4480409a
Ryoyan <553564035@qq.com>
2015-09-07 08:54:37 +0800

    position5

--- Job/app/src/main/java/com/mardin/job/activities/zhiwei/SpinnerTest1.java
+++ Job/app/src/main/java/com/mardin/job/activities/zhiwei/SpinnerTest1.java
@@ -1,117 +1,59 @@
 package com.mardin.job.activities.zhiwei;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.PopupWindow;
-import android.widget.PopupWindow.OnDismissListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.mardin.job.R;
-import com.mardin.job.adapters.zhiwei.PositionSpinnerAdapter;
 
+import com.mardin.job.R;
 
 public class SpinnerTest1 extends Activity {
-    private ArrayList<String> list;
-    //private ImageView imgView;
-    private TextView textView;
-    private LinearLayout layout;
-    private ListView lv;
-    private PositionSpinnerAdapter adapter;
-    private PopupWindow popupWindow;
-    private LinearLayout spinnerlayout;
-    int width;
+
+    private TextView spinnerText;
+    private Spinner Spinner1;
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.abc);
-        textView = (TextView) findViewById(R.id.textView2);
-       // imgView = (ImageView) findViewById(R.id.imageView1);
-        // 实例化一个List,添加数据
-        list = new ArrayList<String>();
-        list.add("全部");
-        list.add("拱墅区");
-        list.add("拱墅区");
-        adapter = new PositionSpinnerAdapter(this, list);
-        textView.setText((CharSequence) adapter.getItem(0));
-        spinnerlayout = (LinearLayout) findViewById(R.id.spinnerid);
-        // 点击右侧按钮，弹出下拉框
-        imgView.setOnClickListener(new OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
+        Spinner1 = (Spinner) findViewById(R.id.Spinner1);
+        spinnerText = (TextView) findViewById(R.id.spinnerText);
 
-                if(list.size()>0){
-                    spinnerlayout.setBackgroundResource(R.drawable.preference_first_item);
-                }
-                showWindow(spinnerlayout, textView);
+        //将可选内容与ArrayAdapter连接起来
+        adapter = ArrayAdapter.createFromResource(this, R.array.area, android.R.layout.simple_spinner_item);
 
-            }
-        });
-    }
-    protected void onResume(){
-        super.onResume();
+        //设置下拉列表的风格
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-    }
-    public void showWindow(View position, final TextView txt) {
+        //将adapter2 添加到spinner中
+        Spinner1.setAdapter(adapter);
 
-        layout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.mypinner_dropdown, null);
-        listView = (ListView) layout.findViewById(R.id.listView);
-        listView.setAdapter(adapter);
-        popupWindow = new PopupWindow(position);
-        // 设置弹框的宽度为布局文件的宽
-        popupWindow.setWidth(spinnerlayout.getWidth());
-        popupWindow.setHeight(LayoutParams.WRAP_CONTENT);
-        // 设置一个透明的背景，不然无法实现点击弹框外，弹框消失
-        popupWindow.setBackgroundDrawable(new BitmapDrawable());
-        // 设置点击弹框外部，弹框消失
-        popupWindow.setOutsideTouchable(true);
-        popupWindow.setFocusable(true);
-        popupWindow.setContentView(layout);
-        // 设置弹框出现的位置，在v的正下方横轴偏移textview的宽度，为了对齐~纵轴不偏移
-        popupWindow.showAsDropDown(position, 0, 0);
-        popupWindow.setOnDismissListener(new OnDismissListener(){
-            @Override
-            public void onDismiss() {
-                // TODO Auto-generated method stub
-                spinnerlayout.setBackgroundResource(R.drawable.preference_single_item);
-            }
+        //添加事件Spinner事件监听
+        Spinner1.setOnItemSelectedListener(new SpinnerXMLSelectedListener());
 
-        });
-        // listView的item点击事件
-        listView.setOnItemClickListener(new OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                // TODO Auto-generated method stub
-                txt.setText(list.get(arg2));// 设置所选的item作为下拉框的标题
-                // 弹框消失
-                popupWindow.dismiss();
-                popupWindow = null;
-            }
-        });
+        //设置默认值
+        Spinner1.setVisibility(View.VISIBLE);
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    //使用XML形式操作
+    class SpinnerXMLSelectedListener implements OnItemSelectedListener{
+        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+                                   long arg3) {
+            spinnerText.setText(""+adapter.getItem(arg2));
+        }
 
+        public void onNothingSelected(AdapterView<?> arg0) {
+
+        }
+
+    }
 }
-
